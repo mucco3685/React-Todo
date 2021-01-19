32f8b4a4a101f43cfe4fb7c30f6940247c9f6a95
mucco3685 <74575320+mucco3685@users.noreply.github.com>
2021-01-20 07:02:51 +0900

    add upper_limit_num

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodo5個までです！</p>
+      )}
 
       <IncompleteTodos
         incompTodo={incompleteTodos}
